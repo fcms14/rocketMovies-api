89688bfaf803fc700d96931f9d888ddf8faa7471
fcms14 <felipecms14@hotmail.com>
2022-06-18 14:29:53 -0300

    SERVER_PORT to PORT

--- src/server.js
+++ src/server.js
@@ -34,5 +34,5 @@ app.use((error, req, res, next) => {
     });
 })
 
-const PORT = process.env.SERVER_PORT || 3333;
+const PORT = process.env.PORT || 3333;
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`))
